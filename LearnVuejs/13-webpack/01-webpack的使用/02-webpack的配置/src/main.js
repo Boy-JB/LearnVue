dbe56c526711aec0968a804bc469388532e25d9f
Little-Stu <1207664552@qq.com>
2020-08-12 21:35:27 +0800

    del

--- LearnVuejs/13-webpack/01-webpack的使用/02-webpack的配置/src/main.js
+++ LearnVuejs/13-webpack/01-webpack的使用/02-webpack的配置/src/main.js
@@ -5,7 +5,7 @@ console.log(add(20, 30));
 console.log(mul(20, 30));
 
 // 2.使用ES6的模块化规范
-import {name, age, height} from "./info.js";
+import {name, age, height} from "./js/info.js";
 
 console.log(name);
 console.log(age);
---
+++ LearnVuejs/13-webpack/01-webpack的使用/04-webpack配置Vue/webpack.config.js
@@ -0,0 +1,83 @@
+const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "dist/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // css-loader只负责将css文件进行加载
+        // style-loader负责将样式添加到DOM中
+        // 使用多个loader时，是从右向左
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: "style-loader", // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader", // translates CSS into CommonJS
+          },
+          {
+            loader: "less-loader", // compiles Less to CSS
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              // 当加载的图片，小于limit时，会将图片编译成base64字符串形式。
+              // 当加载的图片，大于limit时，需要使用file-loader模块进行加载
+              limit: 1024,
+              name: 'img/[name].[hash:8].[ext]'
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.vue$/,
+        use: {
+          loader: ['vue.loader']
+        }
+      }
+      // {
+      //   test: /\.(png|jpg|gif)$/,
+      //   use: [
+      //     {
+      //       loader: 'file-loader',
+      //       options: {}
+      //     }
+      //   ]
+      // }
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  }
+};
