6bf6d698b8aad5990f562c53adf4c240bc98c098
Little-Stu <1207664552@qq.com>
2020-08-18 15:03:01 +0800

    TabBar封装

--- LearnVuejs/16-vue-router/02-tabbar/src/router/index.js
+++ LearnVuejs/16-vue-router/02-tabbar/src/router/index.js
@@ -1,8 +1,40 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import VueRouter from 'vue-router'
+
+const Home = () => import('views/home/Home.vue')
+const Category = () => import('views/category/Category.vue')
+const Cart = () => import('views/cart/Cart.vue')
+const Profile = () => import('views/profile/Profile.vue')
+
+const originalReplace = VueRouter.prototype.replace
+VueRouter.prototype.replace = function replace(location) {
+  return originalReplace.call(this, location).catch(err => err)
+}
 
 Vue.use(Router)
 
 export default new Router({
-  
+  routes: [
+    {
+      path: '',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/Category',
+      component: Category
+    },
+    {
+      path: '/cart',
+      component: Cart
+    },
+    {
+      path: '/profile',
+      component: Profile
+    },
+  ]
 })
